82e0cfad5e6431dac7d71540fa406234c075828b
Eduardo Guerra <eduardo.antares@gmail.com>
2020-06-05 16:18:07 +0000

    Change CreateConfigCommand signature.

--- src/Console/CreateConfigCommand.php
+++ src/Console/CreateConfigCommand.php
@@ -7,9 +7,17 @@ use Illuminate\Support\Str;
 
 class CreateConfigCommand extends Command
 {
-    protected $signature = 'acl:create-config';
+    protected $signature = 'antares:acl-create-config';
 
     protected $description = 'Create ACL package configuration file.';
+
+    protected function relativeToBasePath($path)
+    {
+        if (Str::startsWith($path, base_path())) {
+            return Str::replaceFirst(DIRECTORY_SEPARATOR, '', Str::after($path, base_path()));
+        }
+        return $path;
+    }
 
     public function handle()
     {
@@ -17,16 +25,14 @@ class CreateConfigCommand extends Command
         $sourceFile = "{$targetFile}.template";
 
         if (is_file($targetFile)) {
-            $this->warn('File already exists: '. $targetFile);
-        }
-        else {
+            $this->warn('File already exists: ' . $this->relativeToBasePath($targetFile));
+        } else {
             if (!is_file($sourceFile)) {
-                $this->error('Template file not found: '. $sourceFile);
-            }
-            else {
+                $this->error('Template file not found: ' . $this->relativeToBasePath($sourceFile));
+            } else {
                 $sourceContent = file_get_contents($sourceFile);
                 file_put_contents($targetFile, str_replace('{{jwt_key}}', Str::random(32), $sourceContent));
-                $this->info('Created config file: '. $targetFile);
+                $this->info('Created config file: ' . $this->relativeToBasePath($targetFile));
             }
         }
     }
