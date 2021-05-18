efba7d547689fbc9789a18ac0fb856834ce325dd
Eduardo Guerra <eduardo.antares@gmail.com>
2021-05-18 11:49:05 +0000

    Table acl_groups: turn name attribute unique and create description attribute.

--- database/migrations/2020_03_03_000210_acl_create_acl_groups.php
+++ database/migrations/2020_03_03_000210_acl_create_acl_groups.php
@@ -15,7 +15,8 @@ class AclCreateAclGroups extends Migration
     {
         Schema::create('acl_groups', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
+            $table->string('description')->nullable();
             $table->boolean('enabled')->default(true);
             $table->timestamps();
         });
